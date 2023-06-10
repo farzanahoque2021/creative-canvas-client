e3955c29cb6171d2cb580691517c20d96642d3cc
farzanahoque2021 <farzanahoque2021@gmail.com>
2023-06-10 22:36:17 +0600

    Did design changes in popular instructor section

--- src/pages/Home/PopularInstructor/PopularInstructorCard.jsx
+++ src/pages/Home/PopularInstructor/PopularInstructorCard.jsx
@@ -4,12 +4,15 @@ const PopularInstructorCard = ({ popular }) => {
     const { name, image } = popular;
     return (
         <div>
-            <div className="card h-72 bg-base-100 shadow-lg rounded-xl relative my-4 hover:border-orange-400 hover:border">
+            <div className="group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30 rounded-lg">
+                <div>
+                    <img className="h-80 w-full object-cover transition-transform duration-500 group-hover:rotate-3 group-hover:scale-125" src={image} alt="popular instructor" />
+                </div>
 
-                <figure><img src={image} alt="Popular Instructor" />
-                </figure>
-                <div className="card-body text-center">
-                    <h2 className="card-title mx-auto text-cyan-500 hover:text-orange-500">{name}</h2>
+                <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black flex translate-y-[50%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0 group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70">
+
+                    <p className="text-xl text-white font-bold opacity-0 transition-opacity duration-300 group-hover:opacity-100">{name}</p>
+
                 </div>
             </div>
         </div>
