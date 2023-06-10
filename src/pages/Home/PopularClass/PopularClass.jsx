43e067ad509f6df2688e4b99a4bd2732ce34f69f
farzanahoque2021 <farzanahoque2021@gmail.com>
2023-06-10 15:33:31 +0600

    Did design for popular class section, also fetched data

--- src/pages/Home/PopularClass/PopularClass.jsx
+++ src/pages/Home/PopularClass/PopularClass.jsx
@@ -7,12 +7,14 @@ const PopularClass = () => {
 
 
     return (
-        <div>
-            <h2>Popular Classes</h2>
-            {
-                popularClass.map(popular => <PopularClassCard key={popular._id}
-                    popular={popular}></PopularClassCard>)
-            }
+        <div className="my-8">
+            <h2 className="text-3xl font-bold text-center my-6">Popular Classes</h2>
+            <div className="grid grid-cols-1 lg:grid-cols-3 mx-auto gap-8 w-3/4">
+                {
+                    popularClass.map(popular => <PopularClassCard key={popular._id}
+                        popular={popular}></PopularClassCard>)
+                }
+            </div>
 
         </div>
     );
--- src/pages/Home/PopularClass/PopularClassCard.jsx
+++ src/pages/Home/PopularClass/PopularClassCard.jsx
@@ -1,10 +1,19 @@
 
 const PopularClassCard = ({ popular }) => {
-    const { image, name } = popular;
+    const { image, name, price, studentNumber } = popular;
     return (
         <div>
-            <p><img src={image} alt="" /></p>
-            <p>{name}</p>
+            <div className="card h-96 bg-base-100 shadow-lg rounded-xl relative">
+
+                <figure><img src={image} alt="Shoes" />
+                </figure>
+                <div className="card-body text-center">
+                    <h2 className="card-title mx-auto text-green-400">{name}</h2>
+                    <p className="w-3/12  bg-slate-700 text-white rounded-xl absolute top-0 right-0 opacity-75 px-2">Price: ${price}</p>
+
+                    <p className="text-slate-400">Student Number: {studentNumber}</p>
+                </div>
+            </div>
         </div>
     );
 };
