b45387128c54ee4dd650ab5ef0e13c592c0a6ca8
farzanahoque2021 <farzanahoque2021@gmail.com>
2023-06-11 19:53:27 +0600

    User can successfuly select class and the class is stored in database

--- src/pages/AllClasses/ClassCard.jsx
+++ src/pages/AllClasses/ClassCard.jsx
@@ -1,21 +1,67 @@
 import { useContext } from "react";
 import { AuthContext } from "../../Providers/AuthProvider";
+import Swal from 'sweetalert2'
+import { useLocation, useNavigate } from "react-router-dom";
 
 
 const ClassCard = ({ all }) => {
     const { user } = useContext(AuthContext)
     const { image, _id, name, instructor, seat, price } = all;
-    // card bg-base-100 shadow-xl h-[500px]
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    const handleAddToCart = all => {
+        console.log(all)
+        if (user && user.email) {
+            const cartItem = { classItemId: _id, name, image, price, email: user.email }
+            fetch('http://localhost:5000/carts', {
+                method: 'POST',
+                headers: {
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify(cartItem)
+            })
+                .then(res => res.json())
+                .then(data => {
+                    if (data.insertedId) {
+                        Swal.fire({
+                            position: 'top-end',
+                            icon: 'success',
+                            title: 'Class has been selected',
+                            showConfirmButton: false,
+                            timer: 1500
+                        })
+
+                    }
+                })
+        }
+        else {
+            Swal.fire({
+                title: 'Please login to select the class',
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Login Now'
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    navigate('/login', { state: { from: location } })
+                }
+            })
+
+        }
+    }
+
     return (
         <div>
-            <div className={seat ? "card bg-base-100 shadow-lg h-[500px]" : "card bg-red-300 shadow-lg h-[500px]"}>
+            <div className={seat ? "card bg-base-100 shadow-xl h-[500px]" : "card bg-red-300 shadow-xl h-[500px]"}>
                 <figure><img src={image} alt="all classes" /></figure>
                 <div className="card-body">
                     <h2 className="card-title mx-auto">{name}</h2>
                     <p>Instructor Name: {instructor}</p>
                     <p>Available Seat: {seat}</p>
                     <p>Price: ${price}</p>
-                    <button className="btn btn-accent w-1/3 mx-auto" disabled={user && seat ? false : true}>Select</button>
+                    <button onClick={() => handleAddToCart(all)} className="btn btn-accent w-1/3 mx-auto">Select</button>
 
                 </div>
 
