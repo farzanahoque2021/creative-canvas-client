fdf337344eeb3d3eeccb850e7705b22415e489cf
farzanahoque2021 <farzanahoque2021@gmail.com>
2023-06-12 12:50:42 +0600

    Did delete part from my selected classes

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -14,7 +14,7 @@ const Dashboard = () => {
                 <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
                 <div className="drawer-content flex flex-col items-center justify-center">
                     <Outlet></Outlet>
-                    <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">Open drawer</label>
+                    <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">Open dashboard</label>
 
                 </div>
                 <div className="drawer-side rounded-xl">
--- src/pages/Dashboard/MyCart/MyCart.jsx
+++ src/pages/Dashboard/MyCart/MyCart.jsx
@@ -1,11 +1,43 @@
+import Swal from "sweetalert2";
 import useCart from "../../../hooks/useCart";
+import { FaTrashAlt } from 'react-icons/fa'
 
 
 const MyCart = () => {
-    const [cart] = useCart();
+    const [cart, refetch] = useCart();
+
+    const handleDelete = (item) => {
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                fetch(`http://localhost:5000/carts/${item._id}`, {
+                    method: 'DELETE'
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        if (data.deletedCount > 0) {
+                            refetch();
+                            Swal.fire(
+                                'Deleted!',
+                                'Your class has been deleted.',
+                                'success'
+                            )
+                        }
+                    })
+
+            }
+        })
+
+    }
     return (
-        <div>
-            <h2>Selected Classes</h2>
+        <div className="w-full h-full p-6">
             <div>
                 <h2 className="text-2xl font-bold">Total Classes:  {cart.length}</h2>
 
@@ -20,6 +52,7 @@ const MyCart = () => {
                             </th>
                             <th>Image</th>
                             <th>Name</th>
+                            <th>Price</th>
                             <th>Action</th>
                             <th>Action</th>
                         </tr>
@@ -29,22 +62,23 @@ const MyCart = () => {
                             cart.map((row, index) => <tr key={row._id}>
                                 <td>{index + 1}</td>
                                 <td>
-                                    <div className="flex items-center space-x-3">
-                                        <div className="avatar">
-                                            <div className="mask mask-squircle w-12 h-12">
-                                                <img src={row.image} alt="image" />
-                                            </div>
+
+                                    <div className="avatar">
+                                        <div className="mask mask-squircle w-12 h-12">
+                                            <img src={row.image} alt="image" />
                                         </div>
                                     </div>
+
                                 </td>
                                 <td>
                                     <p className="font-bold">{row.name}</p>
 
                                 </td>
+                                <td><p>${row.price}</p></td>
 
                                 <td><button className="btn btn-outline btn-accent btn-xs">Pay</button></td>
                                 <th>
-                                    <button className="btn btn-outline btn-error btn-xs">Delete</button>
+                                    <button onClick={() => handleDelete(row)} className="btn btn-outline btn-circle btn-error btn-sm"><FaTrashAlt /></button>
                                 </th>
                             </tr>)
                         }
