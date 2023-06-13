2fe85ff079bedccb539afcb7d9a9f2150bb5e224
farzanahoque2021 <farzanahoque2021@gmail.com>
2023-06-14 01:03:54 +0600

    Attempted to do payment

--- src/pages/Dashboard/Payment/Payment.jsx
+++ src/pages/Dashboard/Payment/Payment.jsx
@@ -1,14 +1,20 @@
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
 import { useLoaderData } from "react-router-dom";
+import CheckOutForm from "./CheckOutForm";
 
+const stripePromise = loadStripe(import.meta.env.VITE_Payment_Gateway_PK);
 const Payment = () => {
     const info = useLoaderData();
-    console.log(info)
     const { price } = info;
 
+
     return (
-        <div>
-            <h2>Payment Page</h2>
-            <p>Price:${price}</p>
+        <div className="w-1/2">
+            <h2 className="text-2xl font-bold">Please Proceed</h2>
+            <Elements stripe={stripePromise}>
+                <CheckOutForm info={info} price={price}></CheckOutForm>
+            </Elements>
 
         </div>
     );
