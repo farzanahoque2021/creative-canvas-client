59510ae1d90ce5a05f059cfa3e7ca5e542182942
farzanahoque2021 <farzanahoque2021@gmail.com>
2023-06-14 15:42:40 +0600

    Navbar changes

--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -18,23 +18,23 @@ const Header = () => {
             })
     }
     return (
-        <div className="navbar bg-base-100 px-2 m-0">
+        <div className="navbar relative">
             <div className="navbar-start">
-                <div className="dropdown">
-                    <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                <div className="dropdown absolute z-10">
+                    <label tabIndex={0} className="lg:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
-                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 p-2 shadow bg-white rounded-box w-52">
                         <li><Link to="/">Home</Link></li>
-                        <li><Link>Instructors</Link></li>
-                        <li><Link>Classes</Link></li>
+                        <li><Link to="/instructors">Instructors</Link></li>
+                        <li><Link to="/classes">Classes</Link></li>
                         {
-                            user ? <> <li className='text-base  text-yellow-500'><Link>Dashboard</Link></li></> : ""
+                            user ? <> <li className='text-base  text-yellow-500'><Link to="/dashboard">Dashboard</Link></li></> : ""
                         }
 
                     </ul>
                 </div>
-                <img className='w-40 h-20' src={img} alt="" />
+                <img className='w-40 h-20' src={img} alt="logo" />
 
             </div>
             <div className="navbar-center hidden lg:flex">
